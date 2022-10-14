b4052de07330b0c96ab87d3d7c7acb33b7bb6741
unknown <Savioanjos2020@gmail.com>
2022-10-14 15:01:11 -0300

    After

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import { useState } from 'react'
+
 import './styles/global.css';
+import { Signin } from './pages/Signin';
 
 export function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='font-bold text-2xl text-violet-500'>Hello word</h1>
-
-      <button className='bg-cyan-300 font-medium px-4 py-2 rounded text-white hover:bg-violet-600'>
-        Enviar
-      </button>
-    </>
+    <Signin/>
     
-  )
+  ) 
 }
 
 
--- src/components/MeuCheckbox.tsx
+++ src/components/MeuCheckbox.tsx
@@ -1,11 +1,11 @@
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
 
-export interface CheckboxProps {}
+export interface CheckboxProps extends Checkbox.CheckboxProps {}
  
- export function MeuCheckbox({}: CheckboxProps) {
+ export function MeuCheckbox(props: CheckboxProps) {
     return (
-       <Checkbox.Root className='w-6 h-6 p-[2px] bg-gray-800 rounded'>
+       <Checkbox.Root className='w-6 h-6 p-[2px] bg-gray-800 rounded' {...props}>
           <Checkbox.Indicator asChild>
             <Check weight='bold' className='h-5 w-5 text-cyan-500' />
           </Checkbox.Indicator>
